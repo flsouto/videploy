971123781aa8dbaae33671ca0854d6c8231f8dfe
flsouto <fabiolimasouto@gmail.com>
2023-02-08 14:53:31 -0300

    update

--- vdata.js
+++ vdata.js
@@ -1,5 +1,6 @@
 window.vdata = {
-    "path" : "/home/fabio/Documents/symdrum/symdrumi/02_vid.mp4",
-    "title" : "This is a Test"
+    "path" : "/home/fabio/Documents/symdrum/symdrumiv/03_vid.mp4",
+    "title" : "This is a Test",
+    "description" : "#loop #soundtrack #music #beats\nThis loop was created with artificial inteligence"
 }
 ;
--- yt-uploader.js
+++ yt-uploader.js
@@ -13,16 +13,35 @@ main = async() => {
 
     const btn = await wait_for('#select-files-button .ytcp-button.label');
     if(btn){
+
         // open upload dialog
         btn.click();
         await sleep(2);
+
         // select file from picker
         cmd().enter().exec();
+
         // focus input and paste title
-        ipt = await wait_for('.input-container.title #textbox');
-        ipt.focus();
-        cmd().copy_vdata_to_clipboard("title").paste().exec();
-        ipt.focus();
+        do {
+            t_ipt = await wait_for('.input-container.title #textbox');
+            t_ipt.focus();
+            cmd().copy_vdata_to_clipboard("title").paste().exec();
+            t_ipt.focus();
+            await sleep(5);
+        } while(t_ipt.innerHTML !== vdata.title);
+
+        // focus description input and paste description
+        do{
+            d_ipt = await wait_for("#description-container #textbox");
+            d_ipt.focus();
+            cmd().copy_vdata_to_clipboard("description").paste().exec();
+            d_ipt.focus();
+            await sleep(5);
+        } while(!d_ipt.innerText.trim().length);
+
+        // mark radiobutton "video not made for kids"
+        not_kids_radio = await wait_for("[name=VIDEO_MADE_FOR_KIDS_NOT_MFK] #radioContainer");
+        not_kids_radio.click();
 
         // LANG (todo)
         // find "MOSTRAR MAIS" + Dropdown Icon (click) THEN:
